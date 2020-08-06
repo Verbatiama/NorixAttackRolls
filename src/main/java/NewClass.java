7969fa9cf9b6eca1b05f09fb2cd91ae61377ab5d
Adam H <Verbatiama@gmail.com>
2020-08-06 20:28:06 +0930

    Initial commit
    Code provided by collaborator

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.lang.Math;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ *
+ * @author john
+ */
+
+
+public class NewClass {
+     int d20advRoll[];
+     int d20attack[];
+    public static void main(String args[])
+            
+    { 
+        
+        int max = 20; 
+        int min = 1; 
+        int range = max - min + 1; 
+        // generate random number between 1 and 20 
+        for (int i = 0; i < 1; i++) { 
+            int d20advRoll[]  = new int[] {(int)(Math.random() * range) + min};
+           System.out.println(Math.max(i, i));
+               
+            // Output is different everytime this code is executed
+ 
+            }
+          //  if (Rand1 > Rand2){
+          //                     if (Rand1 == 20){
+          //          System.out.println (Rand1 + " Vorpal") ;
+          //                  }  if (Rand1 >=17 && Rand1<=19) {
+          //          System.out.println (Rand1 + " Crit") ;
+          //                  }  if (Rand1 <=16 && Rand1>=2) {
+         //           System.out.print(Rand1 + " Normal");
+         //                   }  if (Rand1 == 1){
+         //           System.out.print (Rand1 + " Fail");
+                
+
+                 }
+            }
+
+   
+
+
+
+    
